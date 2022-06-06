c3c26d2c61ffdec9e6afea33930a21046fd28b78
vantage-ola <vantageola4@gmail.com>
2022-06-06 12:43:23 +0100

    loading data and renderit to home screen

--- screens/home.js
+++ screens/home.js
@@ -1,13 +1,79 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, {useEffect, useState} from "react";
+import { StyleSheet, Text, View, ActivityIndicator,SafeAreaView } from "react-native";
+import { API_URL,API_KEY } from "../config";
+import MusicSection from "../components/musicSection";
+import { ScrollView } from "react-native-gesture-handler";
 
-export default function HomeScreen() {
-  return (
+
+const initialState = {
+	response: [],
+	next_page: 0
+};
+function randomArtistsGenerator() {
+
+};
+function HomeScreen({ navigation }) {
+
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [data, setData] = useState(initialState);
+
+  const defaultConfig = {
+    method: 'GET',
+    "headers": {
+       "x-rapidapi-host": "genius.p.rapidapi.com",
+       "x-rapidapi-key": `${API_KEY}`,
+   }, 
+   };
+   
+  const handlePress = ({ title, id }) => {};
+
+  useEffect(async() => {
+    setLoading(true);
+    // *insert random artists
+    fetch(`${API_URL}artists/2300/songs?sort=popularity&page=1&per_page=48`, {...defaultConfig} )
+    .then((response) => response.json())
+    .then((data = initialState) => {
+
+
+        setLoading(false);
+        setData(data.response.songs);
+        console.log(data)
+
+        if (error) setError(null);
+    })
+    .catch((e) => {
+        setLoading(false);
+        setError("Fetching movies failed");
+
+    });
+}, []);
+   
+    
+
+  if (loading)
+   return (
     <View styles={styles.container}>
-      <Text>Home Screen</Text>
+      <ActivityIndicator size="large" color="#e8c310"></ActivityIndicator>
     </View>
   );
+
+  if (error) return <Text>ERROR: {error}</Text>;
+
+return (
+  <SafeAreaView>
+    <ScrollView>
+    <MusicSection
+          onPress={handlePress}
+          data={data}
+          title="Featured Songs"
+        ></MusicSection>
+    </ScrollView>
+  </SafeAreaView>
+)
 }
+
+export default HomeScreen;
 
 const styles = StyleSheet.create({
   container: {
@@ -16,4 +82,9 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
   },
+  text: {
+    fontSize: 24,
+    padding: 10,
+    textAlign: "center"
+  }
 });
